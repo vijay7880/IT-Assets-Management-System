e317ad43250611bc5ec5583e12ddff379f05d17a
vijay <ranavijay7880@gmail.com>
2026-09-22 10:05:01 +0530

    Changes

--- src/config/database.js
+++ src/config/database.js
@@ -19,4 +19,25 @@ export async function initializeDatabase() {
   if (userColumns.length === 0) {
     await db.query('ALTER TABLE Users ADD COLUMN email VARCHAR(255) UNIQUE AFTER name');
   }
+
+  await db.query(`CREATE TABLE IF NOT EXISTS Assets(Sno int auto_increment primary key, RDTag_No varchar(20)not null,Asset_Name varchar(200),Category varchar(200)not null,Model varchar(200) not null,
+Serial_No varchar(200) not null, Host varchar(200) not null,Wifi_mac varchar(200) not null,
+Lan_mac varchar(200) not null, IP_Address varchar(200) not null, PO_No varchar(200) not null, Invoice_No varchar(200) not null,Invoice_Date date not null,
+Cost varchar(200) not null,Purchase_Date date not null,Warranty_Date date not null,Status varchar(200))`);
+
+await db.query(`create table if not exists Assigned(RDTag_No varchar(50) not null,E_Name varchar(50) not null,E_ID varchar(50) not null,Department varchar(50) not null,
+Asset_Name varchar(50) not null,Asset_Category varchar(50) not null,Asset_Serial_No varchar(50) not null,
+Asset_Model varchar(50) not null,Assigned_Date date not null)`);
+
+await db.query(`
+create table if not exists Maintenance(Ticket_No varchar(20) unique not null, RDTag_No varchar(50) not null,Asset_Name varchar(50) not null, Asset_Category varchar(50) not null, Asset_Model varchar(50) not null,Asset_Serial_No varchar(50)
+not null,Problem varchar(200) not null, Action varchar(200) not null,Date date not null,Status varchar(50)not null)`);
+
+await db.query(`create table if not exists Scrap(RDTag_No varchar(20)not null,Asset_Name varchar(200)not null,Category varchar(50)not null,Model varchar(50) not null,
+Serial_No varchar(50) not null, Host varchar(50) not null,Wifi_mac varchar(50) not null,
+Lan_mac varchar(50) not null, IP_Address varchar(50) not null, PO_No varchar(50) not null, Invoice_No varchar(50) not null,Invoice_Date date not null,
+Cost varchar(50) not null,Warranty_Date date not null,Purchase_Date date not null,Problem varchar(200),Scrap_Date date)`);
+
+
 }
+
